186dc599834e2c4bb4cd8a0e2c93f589b8dbe57f
warrenniu <warrenniu@gmail.com>
2020-10-16 20:40:19 -0400

    Completed

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,11 @@
 # Create your Costume class here
+class Costume < ActiveRecord::Base
+end
+
 # It should inherit from ActiveRecord::Base
+
+#name
+# price
+# size
+# image url
+# as well as the two "timestamp" columns: created_at and updated_at. This will provide a grand total of six columns.
--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,18 @@
 # Create a class and inherit from ActiveRecord::Migration
+class CreateCostumes < ActiveRecord::Migration[4.2]
+
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :size
+      t.string :image_url
+      t.timestamps 
+    end
+  end
+
+end
+ 
 
 # By convention, the class name should match the part of the
 # file name after the number, so in this case:
